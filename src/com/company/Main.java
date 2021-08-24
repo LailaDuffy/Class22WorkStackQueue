227dbcff1a6036d7e562a3f4a9f67862435f4e44
Laila Duffy <lail.duffy@gmail.com>
2021-08-24 15:18:15 +0100

    Added an if statement to check if stack&queue is empty. Code works.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,37 @@
 package com.company;
 
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input a word/sentence: ");
-        String inputPalindrome = scanner.next();
+        System.out.print("Input a word/sentence: ");
+        String inputPalindrome = scanner.nextLine();
 
         String inputPalindromeLowerCase = inputPalindrome.toLowerCase();
         String inputPalindromeNoSpaces = inputPalindromeLowerCase.replaceAll("\\W", "");
 
-        System.out.println(inputPalindromeNoSpaces);
 
-        PriorityQueue<Character> myQueue = new PriorityQueue<>();
+        Queue<Character> myQueue = new LinkedList<Character>();
         Stack<Character> myStack = new Stack<>();
         for (int i = 0; i < inputPalindromeNoSpaces.length(); i++) {
             myStack.push(inputPalindromeNoSpaces.charAt(i));
             myQueue.add(inputPalindromeNoSpaces.charAt(i));
         }
-        System.out.println(myQueue);
-        System.out.println(myStack);
 
-        for (int i = 0; i < myStack.size(); i++) {
+        for (int i = 0; i < inputPalindromeNoSpaces.length(); i++) {
             if (myStack.lastElement().equals(myQueue.peek())) {
                 myStack.pop();
-                myQueue.remove();
-                System.out.println(myStack.lastElement() + " equals " + myQueue.peek());
-            } else {
-                System.out.println(inputPalindrome + " not a palindrome");
+                myQueue.remove(inputPalindromeNoSpaces.charAt(i));
             }
-
         }
 
+        if (myStack.isEmpty() && myQueue.isEmpty()) {
+            System.out.println(String.format("The word or sentence %s IS a palindrome.", inputPalindrome));
 
+        } else {
+            System.out.println(inputPalindrome + " IS NOT a palindrome");
+        }
     }
 }
